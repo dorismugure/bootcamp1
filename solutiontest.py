0967f9b3bab6882e5af9cd5a8e9cbcfbc1fec892
dorismugure <dorismugure2012@gmail.com>
2017-03-06 19:08:24 +0300

    created solution test

---
+++ solutiontest.py
@@ -0,0 +1,14 @@
+def primes(n): 
+    pr = [] 
+    if type(n) is int:
+    	if n < 0:
+    		return []
+    	elif n > 0:
+    		for num in range(2, n+1):
+    			if all(num % i != 0 for i in range(2, num)):
+    				pr.append(num)
+    		return pr
+    else:
+    	raise TypeError("enter intergers please")
+    
+print(primes(199))
