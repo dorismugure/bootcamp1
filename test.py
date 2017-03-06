730c81c95aab90ac345d8380d4486c77bf607176
dorismugure <dorismugure2012@gmail.com>
2017-03-06 19:21:14 +0300

    update to 9 tests

--- test.py
+++ test.py
@@ -25,6 +25,10 @@ class PrimeNumbersTestCase(unittest.TestCase):
     def test_sixty_one_is_a_prime(self):
         result = primes(97)
         self.assertIn(61, result)
+    def test_output_is_a_list(self):
+        result = primes(43)
+        result = isinstance(result, list)
+        self.assertTrue(result)
 
     def test_return_empty_list_if_n_is_negative(self):
         result = primes(-7)
